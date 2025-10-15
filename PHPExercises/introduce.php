ac326b3573664f99452ce11f8bc4583cbe970073
ucvincemat <vnm8434@students.uc-bcf.edu.ph>
2025-10-15 17:43:05 +0800

    Started form integration

--- PHPExercises/introduce.php
+++ PHPExercises/introduce.php
@@ -1,8 +1,24 @@
+<html>
+<body>
+
+<form action="introduce.php" method="GET">
+Name: <input type="text" name="name"><br>
+Age: <input type="text" name="age"><br>
+Favorite Color: <input type="text" name="favorite_color"><br>
+<input type="submit">
+</form>
+
+<hr>
+<br><br>
+
+</body>
+</html>
+
 <?php
 
-$name = "Vince";
-$age = 20;
-$favorite_color = "blue";
+$name = $_GET["name"] ?? 'Vince';
+$age = $_GET["age"] ?? 22;
+$favorite_color = $_GET["favorite_color"] ?? 'blue';
 
 echo "Hi, I'm <b>$name</b>, I am <b>$age</b> years old, and my favorite color is <b>$favorite_color</b>.";
 
